0961a8b87eda8bdba25d7d141aa5661db88c6cbd
rajeshnagrare <raj.nagrare@gmail.com>
2017-02-03 14:51:31 +0530

    Update Cricket.java

--- Cricket.java
+++ Cricket.java
@@ -4,6 +4,7 @@ public class Cricket {
 	int[] possibleRunsValue = { 0, 1, 2, 3, 4, 6, 0, 1, 2, 3, 1, 0 };
 	String[] possibleBallValue = { "Valid Ball", "Wide Ball", "No Ball", "No Ball", "Valid Ball", "Valid Ball",
 			"Valid Ball", "Wide Ball", "Valid Ball", "Valid Ball", "Wide Ball", "Valid Ball" };
+
 	String[] possibleOuts = { "Not Out", "Bowled", "Not Out", "LBW", "Not Out", "Stumping", "Not Out", "Catch",
 			"Run Out", "Not Out", "Hit Wicket", "Not Out" };
 
@@ -11,7 +12,7 @@ public class Cricket {
 	float totalOvers = (float) 0.0;
 	private int target = 0;
 
-	final int MAX_OVERS = 1;
+	final int MAX_OVERS = 20;
 	private int over;
 	private int ball;
 	private int totalRuns;
@@ -118,29 +119,30 @@ public class Cricket {
 
 	public void displayScore() {
 		System.out.println("Over: " + over + "." + (ball % 6));
-		System.out.println(" ->" + "Current Ball :" + currentBall);
-		System.out.println(" ->" + "Current Out :" + getCurrentOut());
-		System.out.println(" ->" + "Current Runs :" + getCurrentRun());
-		System.out.println(" ->" + "Current Extra Run :" + getCurrentExtraRun());
-		// System.out.println(" ->" + "Total Extra Runs (" + " Wides :" +
-		// getWides() + " No Balls :" + getNoBalls()
-		// + " ) " + getTotalExtraRuns());
-		System.out.println(" ->" + "Team Total Runs :" + getTeamTotalRuns() + "/" + getWickets() + " Extras (w "
-				+ getWides() + ", nb " + getNoBalls() + ") " + getTotalExtraRuns());
+		System.out.println(" ->" + "Current Ball :" + getCurrentBall() + "\t\t" + "Current Out :" + getCurrentOut()
+				+ "\t\t" + "Current Runs :" + getCurrentRun() + "\t\t" + "Current Extra Run :" + getCurrentExtraRun());
+		System.out.println(" ->" + "Team Total Runs :" + getTeamTotalRuns() + "/" + getWickets() + "(" + over + "."
+				+ (ball % 6) + " overs)" + "\t" + " Extras (w " + getWides() + ", nb " + getNoBalls() + ") "
+				+ getTotalExtraRuns());
 		System.out.println(" ->Run Rate :" + getTeamRunRate());
 	}
 
+	public void calculateRunRate() {
+		setTeamRunRate(getTeamTotalRuns() / (getOver() + (getBall() % 6)));
+	}
+
 	public void playMe3() {
-		while (over < MAX_OVERS && wickets < 10) {
+		while (getOver() < MAX_OVERS && getWickets() < 10) {
 			currentBall = generateBall();
 			switch (currentBall) {
 			case "Valid Ball":
 				checkIfOut();
 				if (getWickets() < 10) {
-					ball = ball + 1;
+
+					setBall(getBall() + 1);
 				}
-				if (ball % 6 == 0) {
-					over++;
+				if (getBall() % 6 == 0) {
+					setOver(getOver() + 1);
 				}
 				break;
 			case "Wide Ball":
@@ -157,221 +159,24 @@ public class Cricket {
 			setTotalExtraRuns(getTotalExtraRuns() + getCurrentExtraRun());
 			setTeamTotalRuns(getTotalRuns() + getTotalExtraRuns());
 
-			// setTarget(getTeamTotalRuns());
+			String totalOversString = String.valueOf(getOver()).concat(".").concat(String.valueOf(getBall() % 6));
 
-			// if ((float) (over + ball) == 0.0) {
-			// if (getTeamTotalRuns() == 0) {
-			// setTeamRunRate((float) 0.0);
-			// } else
-			// setTeamRunRate((float) getTeamTotalRuns());
-			//
-			// } else if ((float) (over + ball) < (float) 0.5) {
-			// setTeamRunRate((float) getTeamTotalRuns() / (ball % 6));
-			// } else if ((float) (over + ball) >= (float) 1.0) {
-			// setTeamRunRate((float) getTeamTotalRuns() / ((float) (over)));
-			// }
+			float totalOvers1 = Float.parseFloat(totalOversString);
 
-			// System.out.println(over + "." + (ball % 6) + "\t" + currentBall +
-			// "\t\t" + currentRun + "\t\t" + currentExtraRun
-			// + "\t" + totalExtraRuns);
-			displayScore();
-			setTarget(getTeamTotalRuns());
-			// if (over == MAX_OVERS && wickets == 10 && getTarget() == 0) {
-			// setTarget(getTeamTotalRuns());
-			// }
-			int newTarget=getTarget();
-			
-			if (getTarget() > newTarget) {
-				System.out.println("Team 2 won");
+			// System.out.println("total Overs = " + totalOvers1);
+
+			if (getOver() == 0) {
+				if (getBall() == 0) {
+					setTeamRunRate(getTeamTotalRuns() * 6);
+				} else
+					setTeamRunRate(getTeamTotalRuns() * 6 / (getBall() % 6));
 			} else
-				System.out.println("Team 1 won");
-		}
+				setTeamRunRate((float) getTeamTotalRuns() / totalOvers1);
 
-	}
-	// if (getTeamTotalRuns() >= getTarget()) {
-	// System.out.println("Team 2 won");
-	// } else
-	// System.out.println("Team 1 won");
-
-	public void playMe2() {
-		// System.out.println("Overs" + "\t" + "Current Ball" + "\t" + "Current
-		// Runs" + "\t" + "Current Extra Run" + "\t"
-		// + "Total Extra Run");
-		while (over < MAX_OVERS && wickets < 10) {
-			// if(over<=MAX_OVERS || wickets<=10){
-			// setTarget(getTeamTotalRuns());
-			// }
-
-			currentBall = generateBall();
-			switch (currentBall) {
-			case "Valid Ball":
-				currentOut = generateOuts();
-				setCurrentOut(currentOut);
-				switch (currentOut) {
-				case "Not Out":
-					setCurrentRun(generateRun());
-					setCurrentExtraRun(0);
-					break;
-				case "Bowled":
-					updateOut();
-					break;
-				case "Stumping":
-					updateOut();
-					break;
-				case "LBW":
-					updateOut();
-					break;
-				case "Catch":
-					updateOut();
-					break;
-				case "Hit Wicket":
-					updateOut();
-					break;
-				case "Run Out":
-					updateOut();
-					break;
-				}
-				if (getWickets() < 10) {
-					ball = ball + 1;
-				}
-
-				if (ball % 6 == 0) {
-					over++;
-				}
-				break;
-			case "Wide Ball":
-				updateExtra();
-				setWides(getWides() + 1);
-				break;
-			case "No Ball":
-				updateExtra();
-				setNoBalls(getNoBalls() + 1);
-				break;
-			}
-
-			setTotalRuns(getTotalRuns() + getCurrentRun());
-			setTotalExtraRuns(getTotalExtraRuns() + getCurrentExtraRun());
-			setTeamTotalRuns(getTotalRuns() + getTotalExtraRuns());
-
-			setTarget(getTeamTotalRuns());
-
-			if ((float) (over + ball) == 0.0) {
-				if (getTeamTotalRuns() == 0) {
-					setTeamRunRate((float) 0.0);
-				} else
-					setTeamRunRate((float) getTeamTotalRuns());
-
-			} else if ((float) (over + ball) < (float) 0.5) {
-				setTeamRunRate((float) getTeamTotalRuns() / (ball % 6));
-			} else if ((float) (over + ball) >= (float) 1.0) {
-				setTeamRunRate((float) getTeamTotalRuns() / ((float) (over)));
-			}
-
-			// System.out.println(over + "." + (ball % 6) + "\t" + currentBall +
-			// "\t\t" + currentRun + "\t\t" + currentExtraRun
-			// + "\t" + totalExtraRuns);
-
-			System.out.println("Over: " + over + "." + (ball % 6));
-			System.out.println(" ->" + "Current Ball :" + currentBall);
-			System.out.println(" ->" + "Current Out :" + getCurrentOut());
-			System.out.println(" ->" + "Current Runs :" + getCurrentRun());
-			System.out.println(" ->" + "Current Extra Run :" + getCurrentExtraRun());
-			// System.out.println(" ->" + "Total Extra Runs (" + " Wides :" +
-			// getWides() + " No Balls :" + getNoBalls()
-			// + " ) " + getTotalExtraRuns());
-			System.out.println(" ->" + "Team Total Runs :" + getTeamTotalRuns() + "/" + getWickets() + " Extras (w "
-					+ getWides() + ", nb " + getNoBalls() + ") " + getTotalExtraRuns());
-			System.out.println(" ->Run Rate :" + getTeamRunRate());
-		}
-		System.out.println("Target :" + getTarget());
-	}
-
-	public void playMe() {
-		float maxOvers = (float) 2.0;
-		int ball = 0;
-		float overs = (float) 0.0;
-		while (overs < maxOvers) {
-			currentBall = generateBall();
-			switch (currentBall) {
-			case "Valid Ball":
-				currentRun = generateRun();
-				currentExtraRun = 0;
-				ball = ball + 1;
-				break;
-			}
-			overs = (float) ball / 10;
-			if (ball == 6) {
-				ball = 0;
-				if (overs < 1.0) {
-					overs = (float) 1.0;
-				} else
-					overs = (float) (overs + 1.0);
-			}
-			System.out.println("Over :" + overs);
+			displayScore();
 
 		}
 
-	}
-
-	public void playCricket() {
-		while (totalOvers < (float) MAX_OVERS) {
-			while (i < (float) 0.6) {
-				// for (i = (float) 0.1; i <= (float) 0.6; i += (float) 0.1) {
-				currentBall = generateBall();
-				switch (currentBall) {
-				case "Valid Ball":
-					currentRun = generateRun();
-					currentExtraRun = 0;
-					// totalRuns = totalRuns + currentRun;
-					i += (float) 0.1;
-					over += (float) 0.1;
-					break;
-				case "Wide":
-					// currentRun = cric.generateRun();
-					currentRun = 0;
-					int wideRun = generateRun();
-					i += (float) 0.0;
-					currentExtraRun = 1;
-					if (wideRun != 6) {
-						currentExtraRun = currentExtraRun + wideRun;
-					}
-					wides = wides + currentExtraRun;
-					// totalRuns = totalRuns + currentRun;
-					break;
-				case "No Ball":
-					noBalls = noBalls + 1;
-					currentRun = generateRun();
-					i += (float) 0.0;
-					// currentExtraRun=1;
-					currentExtraRun = currentExtraRun + 1;
-					// totalRuns = totalRuns + currentRun;
-					break;
-				case "Dead Ball":
-					currentRun = 0;
-					currentExtraRun = 0;
-					i += (float) 0.0;
-					// totalRuns = totalRuns + currentRun;
-					break;
-				}
-
-				totalExtraRuns = totalExtraRuns + currentExtraRun;
-				totalRuns = totalRuns + currentRun + currentExtraRun;
-
-				System.out.println("Ball :" + (float) i);
-				System.out.println("|Current Ball: " + currentBall + " |Current Run :" + currentRun
-						+ " |Current Extra Run :" + currentExtraRun);
-				System.out.println("|Total Overs :" + (float) over + " |Total Extra Runs : " + totalExtraRuns
-						+ "|Total Wides :" + wides + "|Total No Balls :" + noBalls + " |Total Runs :" + totalRuns);
-				this.printEndLine();
-				if (i == (float) 0.6) {
-					this.printOver();
-					// over = (float) 1.0;
-
-					// System.out.println((float) overs);
-				}
-			}
-			totalOvers = (float) (totalOvers + over);
-		}
 	}
 
 	public float getI() {
